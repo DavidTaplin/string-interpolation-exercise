06ea73a681e1324de9caf8c707de2bef7fe6de73
DavidTaplin <DTaplin616@live.com>
2022-10-23 22:32:21 -0500

    class 11 exercise 3
    made a name variable gave it a type of string and made it equal to my name.
    
    put a paragraph tag in and with string interpolation made it read "Hello! My name is David!!!!"
    
    also added in very very basic css styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
-  title = 'string-interpolation-exercise';
+  title = 'string interpolation exercise';
+  class = 'class eleven';
+  exercise = 'exercise three';
+
+
+  name: string = ' David ';
 }
